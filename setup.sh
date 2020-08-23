f030ed51eab721adb3f70cb83e41bd200147e824
Simon Piscitelli <simonpiscitelli@gmail.com>
2020-08-23 11:56:32 +0100

    Add bashrc

--- setup.sh
+++ setup.sh
@@ -18,7 +18,8 @@ sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent softwa
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo apt-key fingerprint 0EBFCD88
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt-get update && apt-get install docker-ce docker-ce-cli containerd.io -y
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io -y
 sudo curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo groupadd docker
@@ -28,7 +29,8 @@ echo "<><><><><> <><><><><> <><><><><> DOCKER installed!"
 # with CP
 cp -rf ${BASEDIR}/zsh/oh-my-zsh ~/.oh-my-zsh
 cp -rf ${BASEDIR}/zsh/.p10k.zsh ~/.p10k.zsh
+cp -rf ${BASEDIR}/zsh/.bashrc ~/.bashrc
 cp -rf ${BASEDIR}/zsh/.zshrc ~/.zshrc
 
 mkdir ~/.config/Code && mkdir ~/.config/Code/User
-cp -f ${BASEDIR}/vscode/* ~/.config/Code/User
+cp -rf ${BASEDIR}/vscode/* ~/.config/Code/User
