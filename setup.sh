4573dfd8dc43f91b93522662767627d766f9f68f
Simon Piscitelli <simonpiscitelli@gmail.com>
2020-08-30 14:46:38 +0100

    Add tilt, update bashrc

--- setup.sh
+++ setup.sh
@@ -3,21 +3,24 @@ BASEDIR=$(dirname "$0")
 
 sudo add-apt-repository ppa:linuxuprising/guake
 curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
-sudo apt-get update && apt-get install nodejs zsh curl git guake -y
+sudo apt-get update
+sudo apt-get install nodejs zsh curl git guake -y
 
-## zsh
-echo "<><><><><> <><><><><> <><><><><> Installing ZSH..."
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+## init aliases
 if [ -f ~/.bash_aliases ]; then
     echo "aliases already initialised"
 else
     echo "" > ~/.bash_aliases
 fi
-echo "<><><><><> <><><><><> <><><><><> ZSH installed!"
+
+## zsh
+echo "================================================ Installing ZSH..."
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+echo "================================================ ZSH installed!"
 
 ## docker
-echo "<><><><><> <><><><><> <><><><><> Installing DOCKER..."
+echo "================================================ Installing DOCKER..."
 sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo apt-key fingerprint 0EBFCD88
@@ -28,13 +31,18 @@ sudo curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-
 sudo chmod +x /usr/local/bin/docker-compose
 sudo groupadd docker
 sudo usermod -aG docker $USER
-echo "<><><><><> <><><><><> <><><><><> DOCKER installed!"
+echo "================================================ DOCKER installed!"
 
-## with CP
-cp -rf ${BASEDIR}/zsh/oh-my-zsh ~/.oh-my-zsh
-cp -rf ${BASEDIR}/zsh/.p10k.zsh ~/.p10k.zsh
-cp -rf ${BASEDIR}/zsh/.bashrc ~/.bashrc
-cp -rf ${BASEDIR}/zsh/.zshrc ~/.zshrc
+## tilt
+echo "================================================ Installing Tilt..."
+curl -fsSL https://raw.githubusercontent.com/tilt-dev/tilt/master/scripts/install.sh | bash
+echo "================================================ Tilt installed!"
+
+## CP config files
+## TODO: use ln -s
+cp -rf ${BASEDIR:-.}/zsh/.p10k.zsh ~/.p10k.zsh
+cp -rf ${BASEDIR:-.}/zsh/.bashrc ~/.bashrc
+cp -rf ${BASEDIR:-.}/zsh/.zshrc ~/.zshrc
 
 mkdir ~/.config/Code && mkdir ~/.config/Code/User
-cp -rf ${BASEDIR}/vscode/* ~/.config/Code/User
+cp -rf ${BASEDIR:-.}/vscode/* ~/.config/Code/User
